d71d5e4c71d2576a3d46fef55aa04f0988383103
xyzhenu <1515489649@qq.com>
2019-04-18 22:54:24 +0800

    inhibit warnings

--- XYThirdParty/XYWebVC/XYWebVC.h
+++ XYThirdParty/XYWebVC/XYWebVC.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
 static NSString* keyWebVCGET = @"get";
 static NSString* keyWebVCUrl = @"url";
 static NSString* keyWebVCParma = @"parma";
@@ -15,5 +16,6 @@ static NSString* keyWebVCParma = @"parma";
 -(void)loadFromParma;
 - (void)addWebviewReturnButton:(id _Nullable)content;
 //custom load method with subclass and rewrite this method 
--(void)loadIsGet:(BOOL)isGet url:(NSString*)url parma:(NSDictionary*)parma web:(UIWebView*)web;
+-(void)loadIsGet:(BOOL)isGet url:(NSString*)url parma:(NSDictionary* _Nullable)parma web:(UIWebView*)web;
 @end
+NS_ASSUME_NONNULL_END
