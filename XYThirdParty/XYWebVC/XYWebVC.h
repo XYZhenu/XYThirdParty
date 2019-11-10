55e6433c28878d2458f5d66e2a6a01bd332f6d0c
xie xie <1515489649@qq.com>
2019-11-10 16:58:30 +0800

    update webview methods

--- XYThirdParty/XYWebVC/XYWebVC.h
+++ XYThirdParty/XYWebVC/XYWebVC.h
@@ -19,5 +19,10 @@ static NSString* keyWebVCParma = @"parma";
 - (void)addWebviewReturnButton:(id _Nullable)content;
 //custom load method with subclass and rewrite this method
 -(void)loadIsGet:(BOOL)isGet url:(NSString*)url parma:(NSDictionary* _Nullable)parma web:(WKWebView*)web;
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation;
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler;
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error;
+- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error;
+- (void)webViewWebContentProcessDidTerminate:(WKWebView *)webView;
 @end
 NS_ASSUME_NONNULL_END
